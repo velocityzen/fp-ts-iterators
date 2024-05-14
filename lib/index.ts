3718343da8e59f8c7e570a12c8df9485266177aa
Alexey Novikov <alexey.novikov@reaktor.com>
2024-05-14 17:28:39 -0400

    Add exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,37 @@
 /**
  * @since 1.0.0
  */
+
+/**
+ * @since 1.0.0
+ */
+export * as Iterable from "./Iterable";
+/**
+ * @since 1.0.0
+ */
+export * as Generator from "./Generator";
+
+/**
+ * @since 1.0.0
+ */
 export * as AsyncIterable from "./AsyncIterable";
+
+/**
+ * @since 1.0.0
+ */
 export * as AsyncIterableEither from "./AsyncIterableEither";
+
+/**
+ * @since 1.0.0
+ */
 export * as AsyncIterableOption from "./AsyncIterableOption";
+
+/**
+ * @since 1.0.0
+ */
+export * as AsyncGenerator from "./AsyncGenerator";
+
+/**
+ * @since 1.0.0
+ */
 export * from "./AsyncIterableReduce";
